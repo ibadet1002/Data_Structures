59c8ea70fb8fb8ebcabcb902795c7608647f049d
ibadet1002 <ibadet1002@gmail.com>
2022-02-19 16:31:44 -0500

    Initial commit

---
+++ 00-BigONotation/src/main/java/TimeTest.java
@@ -0,0 +1,14 @@
+public class TimeTest {
+    public static void main(String[] args) {
+        int n = 10000;
+        long startTime = System.currentTimeMillis();
+        int numberOfOperations = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <=n ; j++)
+                numberOfOperations++;
+            }
+            long endTime = System.currentTimeMillis();
+            System.out.println("Number of operations is: " + numberOfOperations + " in " + (endTime - startTime) + " millis");
+
+    }
+}
